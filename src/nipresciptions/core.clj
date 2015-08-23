adde4df51d1963c64614d09f6290abfaab7745ac
Jase Bell <jasebell@gmail.com>
2015-08-23 16:36:38 +0100

    Added function to load practice data from a CSV file. Joined the two RDD's together and alter the function to show the results.

--- src/nipresciptions/core.clj
+++ src/nipresciptions/core.clj
@@ -10,6 +10,17 @@
 
 (def fields [:practice :year :month :vtm_nm :vmp_nm :amp_nm :presentation :strength :total-items :total-quantity :gross-cost :actual-cost :bnfcode :bnfchapter :bnfsection :bnfparagraph :bnfsub-paragraph :noname1 :noname2])
 
+(def practice-fields [:pracno :partnershipno :practicename :address1 :address2 :address3 :postcode :telno :lcg])
+
+(defn load-practices [sc filepath] 
+  (->> (spark/text-file sc filepath)
+       (spark/map #(->> (csv/read-csv %) first))
+       (spark/filter #(not= (first %) "PracNo"))
+       (spark/map #(zipmap practice-fields %))
+       (spark/map-to-pair (fn [rec] 
+                            (let [practice-id (:pracno rec)]
+                              (spark/tuple practice-id rec))))))
+
 (defn load-prescription-data [sc filepath] 
   (->> (spark/text-file sc filepath)
        (spark/map #(->> (csv/read-csv %) first))
@@ -20,18 +31,24 @@
                               (spark/tuple practicekey rec))))
        (spark/group-by-key)))
 
+(defn join-practice-prescriptions [practices prescriptions]
+  (spark/join practices prescriptions))
+
 (defn def-practice-prescription-freq [prescriptiondata]
   (->> prescriptiondata
-       (spark/map-to-pair (s-de/key-value-fn (fn [k v] 
-                                               (let [freqmap (map (fn [rec] (:vmp_nm rec)) v)]
-                                                 (spark/tuple k (apply list (take 10 (reverse (sort-by val (frequencies freqmap))))))))))))
+       (spark/map-to-pair (s-de/key-val-val-fn (fn [k v pr] 
+                                               (let [freqmap (map (fn [rec] (:vmp_nm rec)) pr)]
+                                                 (spark/tuple (str (:practicename v) " " (:postcode v)) (apply list (take 10 (reverse (sort-by val (frequencies freqmap)))))))))))) 
 
-(defn process-data [sc filepath outputpath] 
-  (let [prescription-rdd (load-prescription-data sc filepath)]
-    (->> prescription-rdd
+(defn process-data [sc filepath practicefile outputpath] 
+  (let [prescription-rdd (load-prescription-data sc filepath)
+        practice-rdd (load-practices sc practicefile)]
+    (->> (join-practice-prescriptions practice-rdd prescription-rdd)
          (def-practice-prescription-freq)
          (spark/coalesce 1)
          (spark/save-as-text-file outputpath))))
+
+
 
 (comment 
   (def c (-> (conf/spark-conf)
@@ -42,3 +59,4 @@
 
 
 )
+
